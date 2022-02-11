d23f2239e9481d474c3744393e6dd5224ad49212
codemask <mattode@outlook.com>
2022-02-11 23:22:07 +0100

    user model

--- users/models.py
+++ users/models.py
@@ -1,2 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class User(AbstractUser):
+    WFP = 1
+    UN = 2
+
+    PUBLIC_USER = 1
+    CONTENT_EDITOR = 2
+    ADMINISTRATOR = 3
+
+    AGENCY_CHOICES = [
+        (WFP, "wfp"),
+        (UN, "un")
+    ]
+
+    ROLE_CHOICES = [
+        (PUBLIC_USER, 'public_user'),
+        (CONTENT_EDITOR, "content_editor"),
+        (ADMINISTRATOR, "administrator")
+    ]
+
+    agency = models.CharField(max_length=16, choices=AGENCY_CHOICES, blank=True, null=True)
+    role = models.CharField(max_length=16, choices=ROLE_CHOICES, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
