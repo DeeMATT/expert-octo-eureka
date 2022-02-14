27ebacfd04260a08b60a37b102b99ad4e210b45b
codemask <mattode@outlook.com>
2022-02-14 15:44:37 +0100

    model manager :)

--- users/models.py
+++ users/models.py
@@ -2,6 +2,42 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import EmailValidator
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import UserManager as BaseUserManager
+import string    
+import random 
+
+
+class UserManager(BaseUserManager):
+
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -24,7 +60,6 @@ class User(AbstractUser):
     ]
 
     email_validator = EmailValidator()
-
     email = models.CharField(
         _('email address'),
         max_length=150,
@@ -34,11 +69,14 @@ class User(AbstractUser):
             'unique': _("A user with that email already exists."),
         },
     )
+    username = None
     agency = models.CharField(max_length=16, choices=AGENCY_CHOICES, blank=True, null=True)
     role = models.CharField(max_length=16, choices=ROLE_CHOICES, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
