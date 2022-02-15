9bec3888557af366aefccbe6631c805de4acff29
codemask <mattode@outlook.com>
2022-02-15 15:49:50 +0100

    update

--- wfp_engineering_center/urls.py
+++ wfp_engineering_center/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('content/', include('contents.urls')),
     path('admin/', admin.site.urls),
-    
+    path('/', include('contents.urls')),
 ]
