b7bc4b23da8a7216c83d574f9fc3d535b083160c
DeeMATT <mattode@outlook.com>
2022-02-23 15:09:20 +0100

    urls

--- apps/contents/urls.py
+++ apps/contents/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-from .views import PageListView, PageDetailView
 
+# from .views import PageListView, PageDetailView
+
+# urlpatterns = [
+#     path('pages/', PageListView.as_view()),
+#     path('pages/<slug:slug>/', PageDetailView.as_view()),
+# ]
+
+
+router = DefaultRouter()
+router.register(r'pages', views.PageViewSet)
 
 urlpatterns = [
-    path('pages/', PageListView.as_view()),
-    path('pages/<slug:slug>/', PageDetailView.as_view()),
+    path('', include(router.urls)),
 ]
